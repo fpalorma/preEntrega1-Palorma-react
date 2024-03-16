8eb94581e40990eaeb82268d9811c01246d851bb
fpalorma <140191203+fpalorma@users.noreply.github.com>
2024-03-16 11:57:43 -0300

    se agregó HomeComponent (#7)

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,38 @@
-
+import Carousel from "react-bootstrap/Carousel";
+import "./Home.css";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
     <>
-      <h1>Home del sitio</h1>
+      <Carousel>
+        <Carousel.Item interval={3000}>
+          <img src={"img/coffee-banner-1.jpg"} className={"carouselImg"} />
+          <Carousel.Caption>
+            <h3 className="carouselText">mi-Café - Tienda del barista</h3>
+            <p className="carouselText">
+              Somos importadores y tostadores de café de especialidad.{" "}
+              {<br></br>}Pedí tu variedad favorita y te la llevamos a tu casa.
+            </p>
+            <button>
+              <Link to={"category/café"} className="carouselLinks"> Ver cafés</Link>
+            </button>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={3000}>
+          <img src={"img/coffee-banner-2.jpg"} className={"carouselImg"} />
+          <Carousel.Caption>
+            <h3 className="carouselText">mi-Café - Tienda del barista</h3>
+            <p className="carouselText">
+              Tenemos productos de alta calidad para que armes tu propia
+              cafetería en casa.
+            </p>
+            <button>
+              <Link to={"/products"} className="carouselLinks"> Ver productos</Link>
+            </button>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     </>
   );
 }
