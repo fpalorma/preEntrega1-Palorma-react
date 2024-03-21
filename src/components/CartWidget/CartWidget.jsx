ea28e97414a758bfd70704cb63530d9b73cd2adb
fpalorma <140191203+fpalorma@users.noreply.github.com>
2024-03-21 12:43:47 -0300

    se sincronizó el contador y se creó Cart component (#8)

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,21 +1,27 @@
-
+import { Link } from "react-router-dom";
 
 function CartWidget() {
-
-let estiloCarrito = {
-  background: "transparent",
-  border:"none",
-color:"white",
-fontSize: "1.3em",
-cursor:"pointer"
-}
-    return(
+  let estiloCarrito = {
+    background: "transparent",
+    border: "none",
+    color: "white",
+    fontSize: "1.3em",
+    cursor: "pointer",
+    textDecoration: "none",
+  };
+  return (
     <>
-    <button style={estiloCarrito}><i className="bi bi-cart4"></i></button>
-    
-    <p>0</p>    
+      <button style={estiloCarrito}>
+        {" "}
+        <Link to={"/cart"} style={{ color: "white" }}>
+          {" "}
+          <i className="bi bi-cart4"> </i>
+        </Link>
+      </button>
+
+      <p>0</p>
     </>
-    )
+  );
 }
 
-export default CartWidget
+export default CartWidget;
--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,13 +1,31 @@
 /* eslint-disable react/prop-types */
 import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 import ListGroup from "react-bootstrap/ListGroup";
 import ItemCount from "../ItemCount/ItemCount.jsx";
 import "./ItemDetail.css";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function ItemDetail({ img, name, description, price, stock }) {
+  const [quantityAdded, setQuantity] = useState(0);
+
+  const handleOnAdd = (quantity) => {
+    setQuantity(quantity);
+  };
+
+  const handleOnCancelBtn = () => {
+    setQuantity(0);
+  };
   return (
     <>
-      <Card style={{ width: "22rem", marginTop: "20px" }}>
+      <Card
+        style={{
+          width: "22rem",
+          marginTop: "20px",
+          boxShadow: "10px 5px 20px gray",
+        }}
+      >
         <Card.Img variant="top" src={`../${img}`} alt={name} />
         <Card.Body>
           <Card.Title>{name}</Card.Title>
@@ -17,8 +35,23 @@ export default function ItemDetail({ img, name, description, price, stock }) {
           <ListGroup.Item>Precio: ${price}</ListGroup.Item>
           <ListGroup.Item>Stock disponible: {stock}</ListGroup.Item>
         </ListGroup>
+
         <div className="itemCountContainer">
-          <ItemCount stock={stock} />
+          {quantityAdded > 0 ? (
+            <div className="contenedor-btns">
+              <Button onClick={handleOnCancelBtn}>Cancelar</Button>
+              <Button>
+                <Link
+                  style={{ color: "white", textDecoration: "none" }}
+                  to={"/cart"}
+                >
+                  Ver carrito
+                </Link>
+              </Button>{" "}
+            </div>
+          ) : (
+            <ItemCount stock={stock} onAdd={handleOnAdd} />
+          )}
         </div>
       </Card>
     </>
