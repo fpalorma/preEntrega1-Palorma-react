3c3c1a5a619c55403b36de21a8c36ef68a45b2a3
fpalorma <fedepalorma@gmail.com>
2024-03-21 12:42:40 -0300

    se sincronizó el contador y se creó Cart component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailCont
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home.jsx";
 import Contact from "./components/Contacto/Contacto.jsx";
+import Cart from "./components/Cart/Cart.jsx";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/cart" element={<Cart />} />
           <Route
             exact
             path="/products"
--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import "./ItemCount.css";
 import Button from "react-bootstrap/Button";
 
-export default function ItemCount({ stock }) {
+export default function ItemCount({ stock, onAdd }) {
   let [cont, setCont] = useState(1);
 
   const handleClickSuma = (stock) => {
@@ -12,13 +12,7 @@ export default function ItemCount({ stock }) {
   const handleClickResta = () => {
     cont > 1 ? setCont(cont - 1) : (cont = 1);
   };
-  const handleClickBtnAgregar = () => {
-    if (cont > 1) {
-      console.log("Se agregaron al carrito: " + cont + " productos");
-    } else {
-      console.log("Se agregó al carrito: " + cont + " producto");
-    }
-  };
+  
 
   return (
     <>
@@ -29,11 +23,7 @@ export default function ItemCount({ stock }) {
 
           <button onClick={() => handleClickSuma(stock)}>+</button>
         </div>
-        <Button
-          variant="primary"
-          className="btnAgregar"
-          onClick={handleClickBtnAgregar}
-        >
+        <Button variant="primary" className="btnAgregar" onClick={()=>onAdd(cont)}>
           Agregar al carrito
         </Button>
       </div>
