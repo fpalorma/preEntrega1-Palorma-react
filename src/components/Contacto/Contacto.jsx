6711d9a348ea6aeb8df2c6dfb7a2173d12cd3a19
fpalorma <fedepalorma@gmail.com>
2024-03-30 12:59:45 -0300

    se agregó componente contact

--- src/components/Contacto/Contacto.jsx
+++ src/components/Contacto/Contacto.jsx
@@ -1,9 +1,51 @@
+import "./Contacto.css";
+import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
 
-
-export default function Contacto(){
-    return(
-        <>
-        <h1>Sección de Contacto</h1>
-        </>
-    )
-    }
+export default function Contact() {
+  const { register, handleSubmit } = useForm();
+  const enviar = (data) => {
+    Swal.fire({
+      title: "Mensaje enviado!",
+      text: `Gracias ${data.nombre} por comunicarte con nosotros.`,
+      icon: "success",
+      confirmButtonText: "Volver",
+      color: "peru",
+    });
+  };
+  return (
+    <>
+      <h1>Contáctanos</h1>
+      <form className="formulario" onSubmit={handleSubmit(enviar)}>
+        <label>
+          Nombre:
+          <input
+            type="text"
+            placeholder="Introduce tu nombre"
+            required
+            {...register("nombre")}
+          />
+        </label>
+        <label>
+          Correo:
+          <input
+            type="email"
+            placeholder="Introduce tu correo"
+            required
+            {...register("correo")}
+          />
+        </label>
+        <label>
+          Mensaje:
+          <textarea
+            type="text"
+            placeholder="Déjanos tu mensaje"
+            required
+            {...register("mensaje")}
+          />
+        </label>
+        <button type="submit">Enviar</button>
+      </form>
+    </>
+  );
+}
