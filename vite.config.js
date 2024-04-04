39ed0febb32f2a06dd13224978e15e15c421dc66
fpalorma <fedepalorma@gmail.com>
2024-04-04 12:17:26 -0300

    se configuro el archivo vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  publicDir: "src/assets"
+  publicDir: "src/assets",
+  define: {
+  
+    VITE_FIREBASE_CONFIG: import.meta.env.VITE_FIREBASE_CONFIG,
+  },
 })
